f3ae467faae016a7c61ddcc3b0ea6f3b44d31b43
Maxence <maxence.ogier@gmail.com>
2024-11-22 12:40:17 +0100

    creation projet spring et controller

--- src/main/java/fr/diginamic/hello/restControleurs/HelloRestControleur.java
+++ src/main/java/fr/diginamic/hello/restControleurs/HelloRestControleur.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloRestControleur {
 
     @GetMapping
-    public String Hello() {
+    public String direHello() {
         return "Hello World !";
     }
 }
